c59bbd4aa31ef3b02693be652866d6e3f2bbd899
Ryan Lucas <rlucas@f1r1s13.codam.nl>
2020-07-22 20:36:42 +0200

    nginx working, nothing else
    Nginx dockerfile looks fine. The image is created successfully each
    time, and uses supervisord to ensure that ssh is always running also.
    
    The nginx container can be ssh'd into with the password 'codam2020'.
    
    I've been trying to set up the kubernetes dashboard to work, but after
    hours of endless googling I'm still stuck trying to access it when using
    the LoadBalancer - I'm using the exact same yaml files as recommended,
    and even with the same yaml file as somebody else it just does not seem
    to expose the port correctly - and connections time out every time.
    
    Maybe it will magically work tomorrow.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
-docker build --tag ft_nginx:1.0 srcs/nginx/
-docker run --publish 80:80 --publish 443:443 -it --name ft_nginx ft_nginx:1.0
-# docker run --publish 80:80 --publish 443:443 --detach --name ft_nginx ft_nginx:1.0
+
+# Start the minikube cluster using virtualbox as the driver.
+minikube start --driver=virtualbox
+
+# minikube is running its own docker daemon, use this line to connect
+# to it.
+eval $(minikube -p minikube docker-env)
+
+# This line should set up auto complete options for kubectl
+# source <(kubectl completion zsh)
+
+# First, the namespace metallb-system needs to be created
+kubectl create -f metallb_namespace.yaml
+
+# This line creates a memberlist secret, that is required for RBAC permissions
+# of MetalLB
+kubectl create secret generic -n metallb-system memberlist \
+    --from-literal=secretkey="$(openssl rand -base64 128)"
+
+# Now that the memberlist secret has been created for MetalLB, a MetalLB pod
+# can be created.
+kubectl create -f metallb.yaml
+
+# This is important to do before attaching new pods - create the docker images
+# that the pods will run containers from
+docker build -t rlucas-nginx:1.0 srcs/nginx
+#PasswordAuthentication yes
+
+# Setup the kubernetes dashboard. To get the token to log in, run
+# ./bearer_token.sh
+kubectl create -f dashboard.yaml
+
+kubectl create -f k8s-dashboard-loadbalancer.yaml
+
+# Run nginx server, with ssh enabled
+kubectl create -f nginx.yaml
